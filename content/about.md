fefbcd99eaa43f26e3130f2170dbcc26e20aab3b
Indimeco <18373729+Indimeco@users.noreply.github.com>
2022-07-14 20:28:01 +1000

    add about page

---
+++ content/about.md
@@ -0,0 +1,21 @@
+# About this site
+
+In 1821 in Rome, the poet John Keats lay in his deathbed. He asked his dear friend to inscribe this epitaph upon his tomb:
+
+> Here Lies One Whose Name was writ in Water
+
+There as at once something unaccountably solemn in this epitaph, namely that one's name should fade away in mere moments and be entirely forgotten.
+And yet, there is also something unaccountably attractive about the image of words in water, of the ephemeral and the fleeting and the natural.
+
+We often think of software and the internet as permanent, but they are ephemeral. Try to find websites from the 90s or early 2000s. Hosting companies turn the lights off, DNS servers burn out, hard drives fail.
+The internet is a garden; without gardeners it dissolves.
+
+Here is a pond for me, a patch of mud, a few water lilies. Here is a place where I write in the water.
+
+# About the author
+
+Jacob Lawrence is a United States expatriate living in Sydney Australia.
+
+At 18 he taught himself software development. At 21 he left America to marry his wife in Australia.
+
+He currently works remotely as a software engineer.
---
+++ src/Page/About.elm
@@ -0,0 +1,88 @@
+module Page.About exposing (Data, Model, Msg, page)
+
+import DataSource exposing (DataSource)
+import DataSource.File
+import Head
+import Head.Seo as Seo
+import Html exposing (Html, a, b, div, h1, section, text)
+import Html.Attributes exposing (class, href)
+import Markdown.Parser
+import Markdown.Renderer
+import OptimizedDecoder exposing (Decoder)
+import Page exposing (Page, PageWithState, StaticPayload)
+import Page.Collections exposing (deadEndsToString)
+import Pages.PageUrl exposing (PageUrl)
+import Pages.Url
+import Shared
+import Util.Error exposing (errorMessage)
+import Util.Poem exposing (PoemNode(..), markdownToPoemNodes, timestringToDate)
+import View exposing (View)
+
+
+type alias Model =
+    ()
+
+
+type alias Msg =
+    Never
+
+
+type alias RouteParams =
+    {}
+
+
+type alias Data =
+    String
+
+
+page : Page RouteParams Data
+page =
+    Page.single
+        { head = head
+        , data = data
+        }
+        |> Page.buildNoState { view = view }
+
+
+data : DataSource Data
+data =
+    DataSource.File.bodyWithoutFrontmatter "content/about.md"
+
+
+head :
+    StaticPayload Data RouteParams
+    -> List Head.Tag
+head static =
+    Seo.summary
+        { canonicalUrlOverride = Nothing
+        , siteName = "epitaph"
+        , image =
+            { url = Pages.Url.external "TODO"
+            , alt = "epitaph logo"
+            , dimensions = Nothing
+            , mimeType = Nothing
+            }
+        , description = "TODO"
+        , locale = Nothing
+        , title = "TODO title" -- metadata.title -- TODO
+        }
+        |> Seo.website
+
+
+view :
+    Maybe PageUrl
+    -> Shared.Model
+    -> StaticPayload Data RouteParams
+    -> View Msg
+view maybeUrl sharedModel static =
+    { title = "test"
+    , body =
+        [ Html.section [ Html.Attributes.class "prose" ]
+            (static.data
+                |> Markdown.Parser.parse
+                |> Result.mapError deadEndsToString
+                |> Result.andThen (\ast -> Markdown.Renderer.render Markdown.Renderer.defaultHtmlRenderer ast)
+                |> Result.withDefault [ errorMessage ]
+            )
+        ]
+    }
